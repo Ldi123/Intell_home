beaa118a2074fc8f9a9191faafbb7b857ea20a07
Ldi123 <1670139364@qq.com>
2019-10-09 00:08:53 +0800

    better

--- app/src/main/java/edu/jlu/intell_home/MyPagerAdapter.java
+++ app/src/main/java/edu/jlu/intell_home/MyPagerAdapter.java
@@ -1,64 +1,34 @@
 package edu.jlu.intell_home;
-
 import android.content.Context;
+import android.os.SystemClock;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.viewpager.widget.PagerAdapter;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
-import java.net.UnknownHostException;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 public class MyPagerAdapter extends PagerAdapter {
     private Context mContext;
     private List<String> mData;
     private List<RoomBean>  mList;
     private  Map<String, String> map = new HashMap<>();
     private List<Map<String, String>> listGet = new ArrayList<>();
-
-    String host = "10.46.190.255";// 广播地址
-    int port = 2333;// 广播的目的端口
-    InetAddress adds ;
-    DatagramSocket ds ;
-
-    public void send_mes(String message){//传送要发送的指令
-
-        try {
-
-            adds = InetAddress.getByName(host);//通过ip，输出此对象的 IP 地址字符串和主机名
-            ds = new DatagramSocket();//创建一个实例
-
-            DatagramPacket dp = new DatagramPacket(message.getBytes(),message.length(), adds, port);
-            //构造数据报包，用来将长度为 length 的包发送到指定主机上的指定端口号。
-            System.out.println("123");
-            ds.send(dp);//发送信息
-            System.out.println("456");
-        }
-        catch (UnknownHostException e) {
-            e.printStackTrace();
-            System.out.println("11111");
-        }
-        catch (SocketException e) {
-            e.printStackTrace();
-            System.out.println("2222");
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("3333");
-        }
-    }
-
+    private Socket client = null;  //建立客户端
+    private DataOutputStream dos;  //TCP通信数据输出流
+    private DataInputStream dis;   //TCP通信数据输入流
+    private String msg;
+    private  int rec;
     public MyPagerAdapter(Context context ,List<String> list,List<RoomBean>  it_List) {
         mContext = context;
         mData = list;
@@ -96,38 +66,77 @@ public class MyPagerAdapter extends PagerAdapter {
         TextView tv = view.findViewById(R.id.tv);
         final TextView text = view.findViewById(R.id.text);
         final Switch sw1=view.findViewById(R.id.switch1);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                ThreadCreClient();
+            }
+        }).start();
+        SystemClock.sleep(300);
+        if(client==null||!client.isConnected()) {
+            Toast.makeText(mContext,"TCP连接不成功",Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(mContext,"TCP连接成功",Toast.LENGTH_SHORT).show();
+        }
         sw1.setText(s3);
         sw1.setId(s2.charAt(0));
         sw1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             //监听开关的改变
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean is_checked) {
-                if(is_checked){                 //可以理解为点击开启
-                send_mes("Turn on1");
+                if(is_checked){
+                    if(client==null||!client.isConnected()) {
+                        Toast.makeText(mContext,"状态改变！但设备未连接",Toast.LENGTH_SHORT).show();
+                        }else{
+                        if(null!=dos) {
+                            new Thread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    msg = "f";
+                                    send(msg);
+                                }
+                            }).start();
+                        }
+                        Toast.makeText(mContext,"Turn on",Toast.LENGTH_SHORT).show();
+                    }
+                } else{
+                    if(client==null||!client.isConnected()) {
+                        Toast.makeText(mContext,"状态改变！但设备未连接",Toast.LENGTH_SHORT).show();
+                        }else{
+                        if(null!=dos) {
+                            new Thread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    msg = "o";
+                                    send(msg);
+                                }
+                            }).start();
+                        }
+                        Toast.makeText(mContext,"Turn off",Toast.LENGTH_SHORT).show();
+                    }
                 }
-                else{
-                send_mes("Turn off1");
-                }
+
             }
         });
         tv.setText(mData.get(position));
+        text.append("第"+(position+1)+"个房间的定制信息\n");
+        text.append("---------------------\n");
         text.append("Room_id:"+s0 + "\n");
         text.append("Room_name:"+s1 + "\n");
         text.append("Device_id:" + listGet.get(position).get("Device_id")+ "\n");
-        text.append("Device_name" + listGet.get(position).get("Device_name") + "\n");
+        text.append("Device_name:" + listGet.get(position).get("Device_name") + "\n");
         text.append("IP:"+s4 + "\n");
         text.append("Port:"+s5 + "\n");
         text.append("Type:"+s6 + "\n");
-        text.append("state:"+s7 + "\n");
-        text.append("code:"+s8 + "\n");
-        text.append("-----------------\n");
+//        text.append("state:"+s7 + "\n");
+//        text.append("code:"+s8 + "\n");
+        text.append("---------------------\n");
         container.addView(view);
         return view;
     }
 
     @Override
     public void destroyItem(ViewGroup container, int position, Object object) {
-        // super.destroyItem(container,position,object); 这一句要删除，否则报错
         container.removeView((View)object);
     }
 
@@ -140,7 +149,39 @@ public class MyPagerAdapter extends PagerAdapter {
         return mData.get(position);
     }
 
+    public void ThreadCreClient(){
+        while(client == null||!client.isConnected()){
+            try {
+                client = new Socket("192.168.1.73",80);
+                if(client.isConnected()) {//TCP连接成功
+                    createIO();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                SystemClock.sleep(1000*2);
+            }
+        }
+    }
+
+    public void createIO(){
+
+        try {
+            dos = new DataOutputStream(client.getOutputStream());
+            dis = new DataInputStream(client.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+            //relesas();//如果IO流建立失败用于释放资源
+        }
+    }
+
+    public void send(String msg){
+        try {
+            dos.writeUTF(msg);
+            dos.flush();
+            rec=dis.read();
+            System.out.println(rec);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
-
-
-
